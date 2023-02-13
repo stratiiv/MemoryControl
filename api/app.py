ef564edc07257e09d39515a878756a25b8292ef5
stratiiv <stratiyvalter@gmail.com>
2023-02-13 20:42:32 +0200

    implement get, post, put methods

--- api/app.py
+++ api/app.py
@@ -1,30 +1,57 @@
 from flask import Flask,request,jsonify
-from flask_pymongo import PyMongo
-import os 
+from pymongo import MongoClient
+from bson import json_util
+import json
+
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-mongodb_client = PyMongo(app)
-db = mongodb_client.db
+mongodb_client = MongoClient('mongodb://root:pass@localhost:27018/')
+db = mongodb_client.test_db
+coll = db.test_collection
 
-
-@app.route('/',methods = ['GET'])
+@app.route('/api/v1/list',methods = ['GET'])
 def get_data():
-    return jsonify({'message':'hello world'})
+    try:
+        if coll.find().count() > 0:
+            return json.loads(json_util.dumps([obj for obj in coll.find()]))
+        else:
+            return jsonify([])
+    
+    except:
+        return jsonify({'message':'Error while trying to fetch the resource'}), 500
+ 
 
-@app.route('/',methods = ['POST'])
+@app.route('/api/v1/list',methods = ['POST'])
 def post_data():
-    return jsonify({'message':'post hello world'})
-
-@app.route('/',methods = ['PUT'])
-def put_data():
-    return jsonify({'message':'put hello world'})
-
-
-
-
-
-
-
+    try:
+        try:
+            body = request.get_json()
+        except:
+            return jsonify({'message':'Bad request body'}),400
+            
+        ins_result = coll.insert_one(body)
+        return jsonify({'message':'Sucess!'})
+    except:
+        return jsonify({'message':'Error while trying to fetch the resource'}), 500
+    
+@app.route('/api/v1/list/<obj_id>',methods = ['PUT'])
+def put_data(obj_id):
+    print(obj_id)
+    try:
+        try:
+            body = request.get_json()
+        except:
+            return jsonify({'message':'Bad request body'}), 400
+        
+        upd_result = coll.update_one({"id":int(obj_id)},body)
+        if upd_result.modified_count > 0:
+            return jsonify({'message':'Updated sucessfully'}), 200
+        else:
+            return jsonify({'message':'Not available to update'}), 404
+    
+    except Exception as e:
+        print(e)
+        return jsonify({'message':'Error while trying to fetch the resource'}), 500
+ 
 if __name__=='__main__':
     app.run(host='127.0.0.1',port=8080,debug=True)
